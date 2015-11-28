67b8b65fcda8fc61da50293a45b696733abb19c6
ghjuvan <vaevconsult@gmail.com>
2015-11-28 15:48:51 +0100

    init

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 var gulp = require('gulp');
-var server = require('gulp-server-livereload');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
-gulp.task('webserver', function () {
-    gulp.src('app')
-        .pipe(server({
-            livereload: true,
-            directoryListing: true,
-            open: true,
-            defaultFile: './app/index.html'
-        }));
+// watch files for changes and reload
+gulp.task('serve', function() {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    }
+  });
+
+  gulp.watch(['**/*.html', '**/*.css', '**/*.js'], {cwd: 'app'}, reload);
 });
